4be87944edb859963c14c131b43884c256edc8a1
huanglulu <lulu.huanglulu@alibaba-inc.com>
2023-03-16 22:01:47 +0800

    123

---
+++ src/main/java/main.java
@@ -0,0 +1,14 @@
+/**
+ * Alibaba.com Inc.
+ * Copyright (c) 1999-2023 All Rights Reserved.
+ */
+
+/**
+ * @author lulu.huanglulu@alibaba-inc.com
+ * @date 2023/03/16
+ */
+public class main {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
